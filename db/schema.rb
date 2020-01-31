693b6a3879eba4634b28df8ec5e3b0a2f2f7342d
Yair Facio <yair.facio11@gmail.com>
2020-01-31 16:16:16 -0600

    Created the user model and added presence validation, added hirb to the gem file

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_31_215550) do
+ActiveRecord::Schema.define(version: 2020_01_31_221033) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
